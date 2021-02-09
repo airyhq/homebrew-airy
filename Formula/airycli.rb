d90aa611fb60f29ccb861c412b0301cb3f19c018
Pascal Holy <pascal@airy.co>
2021-02-09 17:58:11 +0100

    Initial formula commit

---
+++ Formula/airycli.rb
@@ -0,0 +1,11 @@
+class Airycli < Formula
+  desc "helps create, manage, and test an Airy Core instance"
+  homepage "https://airy.co"
+  url "https://airy-core-binaries.s3.amazonaws.com/0.8.0/darwin/amd64/airy"
+  sha256 "55be8c37dfdd816befb90e0d88a13bf2b6418ae28c7574eb38e53ed32bd5f4a1"
+  license ""
+
+  def install
+    bin.install "airy"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Airyhq Airycli
+
+## How do I install these formulae?
+`brew install airyhq/airy/<formula>`
+
+Or `brew tap airyhq/airy` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
