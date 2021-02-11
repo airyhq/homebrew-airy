2594fc0fcd3a828982386b49fed18508fec8c5be
Pascal Holy <pascal@airy.co>
2021-02-11 14:04:11 +0100

    Fix licence str

--- Formula/airy-cli.rb
+++ Formula/airy-cli.rb
@@ -3,7 +3,7 @@ class AiryCli < Formula
   homepage "https://airy.co"
   url "https://airy-core-binaries.s3.amazonaws.com/0.8.0/darwin/amd64/airy"
   sha256 "55be8c37dfdd816befb90e0d88a13bf2b6418ae28c7574eb38e53ed32bd5f4a1"
-  license "Apache License 2.0"
+  license "Apache-2.0"
 
   def install
     bin.install "airy"
