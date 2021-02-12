7417918e408263224331389f9949d49386ab66d9
Pascal Holy <pascal@airy.co>
2021-02-12 10:17:20 +0100

    Rename Formula

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,4 +1,4 @@
-class AiryCli < Formula
+class Cli < Formula
   desc "Helps create, manage, and test an Airy Core instance"
   homepage "https://airy.co"
   url "https://airy-core-binaries.s3.amazonaws.com/0.8.0/darwin/amd64/airy"
