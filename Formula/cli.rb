74b4e9eaee0a819b7723b8169fbf32147cc5271b
Ljupco Vangelski <ljupcovangelski@users.noreply.github.com>
2021-03-24 11:59:54 +0100

    Update sha for the last time for 0.14.0

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -2,7 +2,7 @@ class Cli < Formula
   desc "Helps create, manage, and test an Airy Core instance"
   homepage "https://airy.co"
   url "https://airy-core-binaries.s3.amazonaws.com/0.14.0/darwin/amd64/airy"
-  sha256 "df2d02d22062079ad8e8246b4da8c3db4b830b11773d1be462cd3a0c44111064"
+  sha256 "4ed345ce0e0f9f685a4fc70dccd0d0b3cad9b1d0a44a8651b6b20517ad065aa8"
   license "Apache-2.0"
 
   def install
