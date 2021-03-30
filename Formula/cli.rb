68df428771d6a5775703e9f8a6603623dfa8b6ea
Pascal Holy <pascal@airy.co>
2021-03-30 14:25:23 +0200

    Fix sha

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -2,7 +2,7 @@ class Cli < Formula
   desc "Helps create, manage, and test an Airy Core instance"
   homepage "https://airy.co"
   url "https://airy-core-binaries.s3.amazonaws.com/0.15.0/darwin/amd64/airy"
-  sha256 "16e9a8164c8642ad6dd993d7ddefaeaca693e9fd265b1963474bf5f719b7a0d3"
+  sha256 "9ac0118b8f723223e9e4f2ef41c04f904b665664a76306d3f809d577069e9a0a"
   license "Apache-2.0"
 
   def install
