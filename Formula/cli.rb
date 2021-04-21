061f720bca211ce9d2a53a6dd74eb3379fdd5d2d
Pascal Holy <pascal@airy.co>
2021-04-21 11:41:16 +0200

    Release 0.18.0

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,8 +1,8 @@
 class Cli < Formula
   desc "Helps create, manage, and test an Airy Core instance"
   homepage "https://airy.co"
-  url "https://airy-core-binaries.s3.amazonaws.com/0.17.0/darwin/amd64/airy"
-  sha256 "5fcf334e88924d045568a9058df3e5e713ab27759a420820990431b762629d39"
+  url "https://airy-core-binaries.s3.amazonaws.com/0.18.0/darwin/amd64/airy"
+  sha256 "12bbd1bf1cc8eeaf1c1345bc05f9beb0a226fbabf3fed82b04d3657be0a042e5"
   license "Apache-2.0"
 
   def install
